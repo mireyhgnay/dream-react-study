4fa1ba526c7ef01dda1ca95929d4c245644c014f
hyerim <hyerimiya1216@gmail.com>
2023-03-28 21:41:01 +0900

    Feat : 신규 컴포넌트 생성

---
+++ dream-react-cra/src/AppProfile.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Profile from "./components/Profile";
+
+// JSX 문법 공부 문서
+function AppProfile() {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+}
+
+export default AppProfile;
