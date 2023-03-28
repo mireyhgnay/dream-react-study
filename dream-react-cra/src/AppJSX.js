33762eefa88999efec1aebb2862dc0892991b246
hyerim <hyerimiya1216@gmail.com>
2023-03-28 21:37:05 +0900

    Feat : AppJSX 파일로 변경

---
+++ dream-react-cra/src/AppJSX.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+// JSX 문법 공부 문서
+function AppJSX() {
+  const name = "hyerim";
+  const list = ["우유", "딸기", "바나나", "요거트"];
+  return (
+    <>
+      <h1 className="orange">{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img
+        style={{ width: "200px", height: "200px" }}
+        src="https://plus.unsplash.com/premium_photo-1674331710614-41eab5c75aab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80"
+        alt="nature"
+      />
+    </>
+  );
+}
+
+export default AppJSX;
