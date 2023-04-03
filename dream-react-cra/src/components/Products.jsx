16ca9629fb8509947122a42e16e4b7e7a4e74420
hyerim <hyerimiya1216@gmail.com>
2023-04-03 23:25:52 +0900

    Feat : useEffect 체크될때마다 데이터 변경하기

--- dream-react-cra/src/components/Products.jsx
+++ dream-react-cra/src/components/Products.jsx
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from 'react';
 export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false); // 처음엔 체크되어있지 않도록 초기값 설정
+  const handleChange = () => setChecked((prev) => !prev); // 이전 값을 반대로 토글링 할 수 있도록 한다
 
   useEffect(() => {
-    fetch("data/products.json") // 상대경로를 쓰면 바로 public폴더로 접근하게 된다
+    fetch(`data/${checked ? 'sale_' : ''}products.json`) // 상대경로를 쓰면 바로 public폴더로 접근하게 된다
       .then((res) => res.json()) // 응답을 받은 걸 json 형태로 변환
       .then((data) => {
         // data 를 받아옴
@@ -16,10 +18,18 @@ export default function Products() {
       // 데이터가 화면에서 사라질때 호출되는 함수
       console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
-  }, []);
+  }, [checked]); // checked가 변경될 때마다 수행한다
 
   return (
     <>
+      {/* 체크박스가 체크될 때는 세일 데이트를 가져온다. */}
+      <input 
+        id="checkbox"
+        type="checkbox" 
+        value={checked} 
+        onChange={handleChange} 
+      />
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map(({ id, name, price }) => (
           <li key={id}>
