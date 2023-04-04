e3a4f98651ba47ee33d699b6faebc1e9369e3575
hyerim <hyerimiya1216@gmail.com>
2023-04-04 23:49:37 +0900

    Feat : 커스텀훅

---
+++ dream-react-cra/src/hooks/use-products.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+// 커스텀 훅 : 외부 사람들과 공유하고 싶은 데이터를 리턴한다
+// (기존 컴포넌트는 ui를 리턴)
+export default function useProducts({ salesOnly }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        setProducts(data);
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
+    return () => {
+      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
+    };
+  }, [salesOnly]);
+
+  return [loading, error, products];
+}
